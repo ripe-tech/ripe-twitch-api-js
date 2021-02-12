c7878a555ae616fd55369067429da83a039efd2d
BeeMargarida <ms@platforme.com>
2021-02-12 09:38:43 +0000

    fix: using Options from yonius

--- types/order.d.ts
+++ types/order.d.ts
@@ -1,17 +1,12 @@
-export interface Options {
-    readonly fields?: string[],
-    readonly eager?: boolean,
-    readonly eagerL?: boolean,
-    readonly map?: boolean,
-    readonly rules?: boolean,
-    readonly meta?: boolean,
-    readonly build?: boolean,
-    readonly fill?: boolean,
-    readonly resolveA?: boolean,
-    readonly skip?: number,
-    readonly limit?: number,
-    readonly sort?: unknown[][],
-    readonly raiseE?: boolean,
+import { Options } from "yonius";
+
+export interface Address {
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly address: string;
+    readonly postcode: string;
+    readonly country: string;
+    readonly state: string;
 }
 
 export enum Status {
