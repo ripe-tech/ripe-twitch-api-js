20a1980f16faef2d85104b2043b4ca737111cd8d
BeeMargarida <ms@platforme.com>
2021-03-03 11:58:00 +0000

    feat: new data models

---
+++ js/collection.js
@@ -0,0 +1,75 @@
+/**
+ * @class
+ * @classdesc API for the Collection entities.
+ */
+export const CollectionAPI = superclass =>
+    class extends superclass {
+        /**
+         * Returns all collections.
+         *
+         * @memberof CollectionAPI
+         * @param {Object} options An object of options to configure the query and
+         * its results.
+         * @returns {Promise} The collection result list.
+         */
+        async listCollections(options = {}) {
+            const url = this.baseUrl + "collections";
+            const contents = await this.get(url, options);
+            return contents;
+        }
+
+        /**
+         * Creates a new collection with the provided information.
+         *
+         * @memberof CollectionAPI
+         * @param {Collection} payload An object that contains information about an collection.
+         * @returns {Promise} The created collection.
+         */
+        async createCollection(payload) {
+            const url = this.baseUrl + "collections";
+            const collection = await this.post(url, { dataJ: payload });
+            return collection;
+        }
+
+        /**
+         * Returns the collection with the provided id.
+         *
+         * @memberof CollectionAPI
+         * @param {String} id The id of the collection.
+         * @returns {Promise} The collection requested.
+         */
+        async getCollection(id) {
+            const url = this.baseUrl + `collections/${id}`;
+            const collection = await this.get(url);
+            return collection;
+        }
+
+        /**
+         * Updates the collection with the provided information.
+         *
+         * @memberof CollectionAPI
+         * @param {String} id The id of the collection.
+         * @param {Object} payload An object that contains information about an collection.
+         * @returns {Promise} The updated collection.
+         */
+        async updateCollection(id, payload) {
+            const url = this.baseUrl + `collections/${id}`;
+            const collection = await this.put(url, { dataJ: payload });
+            return collection;
+        }
+
+        /**
+         * Deletes the collection with the provided id.
+         *
+         * @memberof CollectionAPI
+         * @param {String} id The id of the collection.
+         * @returns {Promise} Empty response.
+         */
+        async deleteCollection(id) {
+            const url = this.baseUrl + `collections/${id}`;
+            const collection = await this.delete(url);
+            return collection;
+        }
+    };
+
+export default CollectionAPI;
---
+++ js/product-drop.js
@@ -0,0 +1,75 @@
+/**
+ * @class
+ * @classdesc API for the ProductDrop entities.
+ */
+export const ProductDropAPI = superclass =>
+    class extends superclass {
+        /**
+         * Returns all product of a drop.
+         *
+         * @memberof ProductDropAPI
+         * @param {Object} options An object of options to configure the query and
+         * its results.
+         * @returns {Promise} The product result list.
+         */
+        async listProductDrops(options = {}) {
+            const url = this.baseUrl + "product_drops";
+            const contents = await this.get(url, options);
+            return contents;
+        }
+
+        /**
+         * Creates a new product with the provided information.
+         *
+         * @memberof ProductDropAPI
+         * @param {ProductDrop} payload An object that contains information about an product.
+         * @returns {Promise} The created product.
+         */
+        async createProductDrop(payload) {
+            const url = this.baseUrl + "product_drops";
+            const productDrop = await this.post(url, { dataJ: payload });
+            return productDrop;
+        }
+
+        /**
+         * Returns the product with the provided id.
+         *
+         * @memberof ProductDropAPI
+         * @param {String} id The id of the product.
+         * @returns {Promise} The product requested.
+         */
+        async getProductDrop(id) {
+            const url = this.baseUrl + `product_drops/${id}`;
+            const productDrop = await this.get(url);
+            return productDrop;
+        }
+
+        /**
+         * Updates the product with the provided information.
+         *
+         * @memberof ProductDropAPI
+         * @param {String} id The id of the product_drop.
+         * @param {Object} payload An object that contains information about an product.
+         * @returns {Promise} The updated product.
+         */
+        async updateProductDrop(id, payload) {
+            const url = this.baseUrl + `product_drops/${id}`;
+            const productDrop = await this.put(url, { dataJ: payload });
+            return productDrop;
+        }
+
+        /**
+         * Deletes the product with the provided id.
+         *
+         * @memberof ProductDropAPI
+         * @param {String} id The id of the product.
+         * @returns {Promise} Empty response.
+         */
+        async deleteProductDrop(id) {
+            const url = this.baseUrl + `product_drops/${id}`;
+            const productDrop = await this.delete(url);
+            return productDrop;
+        }
+    };
+
+export default ProductDropAPI;
