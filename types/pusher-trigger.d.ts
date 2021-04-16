30cde8fa450f86351b1959095bd2ca4aef8dea50
3rdvision <kazgru@gmail.com>
2021-04-16 11:23:56 +0100

    refactor: using response from node-fetch

--- types/pusher-trigger.d.ts
+++ types/pusher-trigger.d.ts
@@ -1,14 +1,11 @@
+import { Response } from "node-fetch";
+
 export interface PusherTrigger {
     readonly channel: string;
     readonly event: string;
     readonly payload: string;
 }
 
-export interface PusherTriggerResponse {
-    readonly size: number;
-    readonly timeout: number;
-}
-
 export declare class PusherTriggerAPI {
-    pusherTrigger(payload: PusherTrigger): Promise<PusherTriggerResponse>;
+    pusherTrigger(payload: PusherTrigger): Promise<Response>;
 }
